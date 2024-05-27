75f3f5f50d1b153dc36e5897d318211f2e7d99d7
eaa23cofa <eaa23cofa@students.eaaa.dk>
2024-05-27 12:31:52 +0200

    Done

--- app.js
+++ app.js
@@ -3,6 +3,52 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const posts = await getPosts();
+  console.log(posts);
+  displayPosts(posts);
+  displayPostsGrid(posts);
+}
+
+//fetch wordpress posts
+async function getPosts() {
+  const response = await fetch(
+    "https://headless.cederdorff.dk/wp-json/wp/v2/posts?acf_format=standard"
+  );
+    const data = await response.json();
+    return data;
+}
+
+//display posts
+function displayPosts(posts) {
+
+  const postsList = document.querySelector("#posts-list");
+
+  for (const post of posts) {
+    postsList.insertAdjacentHTML(
+      "beforeend",
+      /*html*/ `
+      <li>${post.title.rendered}</li>
+        `
+        );
+  }
+}
+
+//display dinamic grid of posts
+function displayPostsGrid(posts){
+  const postsGrid = document.querySelector("#posts-grid");
+
+
+for (const post of posts) {
+  postsGrid.insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `
+    <article class="grid-item">
+      <img src="${post.acf.image}" alt="${post.title.rendered}" />
+      <h2>${post.title.rendered}</h2>
+    </article>
+  `
+  );
+}
 }
